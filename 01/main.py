c821ff7f622f3828e700ec06350d3e1d17e325b1
n0rrman <email@henriknorrman.com>
2023-12-01 20:39:54 +0700

    day 01 complete

--- 01/main.py
+++ 01/main.py
@@ -10,21 +10,77 @@ def read_file(file_name):
     return content
 
 def process_input(input):
-    data = pd.DataFrame([input.splitlines()])
-    return data.T.astype(int)
+    data = input.splitlines()
+    return data
+
+
+def calc_part_one(data):
+    first = False
+    last = False
+
+    sum = 0
+
+    for row in data:
+        for char in row:
+            try:
+                if not first:
+                    first = int(char)
+                    last = int(char)
+                else:
+                    last = int(char)
+            except:
+                pass
+        sum += (int(str(first) + str(last)))
+        first = False;
+    
+    return sum
+
+
+
+def calc_part_two(data):
+    letter_to_int = {
+        "one": "one1one",
+        "two": "two2two",
+        "three": "three3three",
+        "four": "four4four",
+        "five": "five5five",
+        "six": "six6six",
+        "seven": "seven7seven",
+        "eight": "eight8eight",
+        "nine": "nine9nine"
+    }
+
+    first = False
+    last = False
+
+    sum = 0
+
+    for row in data:
+        new_row = row
+        for k, v in letter_to_int.items():
+            new_row = new_row.replace(k, str(v))
+        for char in new_row:
+            try:
+                if not first:
+                    first = int(char)
+                    last = int(char)
+                else:
+                    last = int(char)
+            except:
+                pass
+        sum += (int(str(first) + str(last)))
+        first = False;
+    return sum
+
+
 
 def main():
-    test_input = read_file('test_input.txt')
-    test_data = process_input(test_input)
-    test_answer = 6
-    
-    part_one = test_data.sum()[0]
-    
-    assert part_one == test_answer
+    input = read_file('input.txt')
+    data = process_input(input)
 
+    part_one = calc_part_one(data)
+    part_two = calc_part_two(data)
 
-    part_one = 0 
-    part_two = 0
 
     print("Part one: {}\nPart two: {}".format(part_one, part_two))
 
